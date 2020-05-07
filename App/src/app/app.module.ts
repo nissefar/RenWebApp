b8b6cb05038465495eae4419d738ff5eb74cf834
nissefar <nikolajharder@live.dk>
2020-05-07 11:04:20 +0200

    removed a compnent

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MemberComponent } from './member/member.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MemberComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
